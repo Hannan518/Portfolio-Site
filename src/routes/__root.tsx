9f08ba4ce0def0dc6297a178f718571977e371d7
Hannan518 <hannannajeeb6@gmail.com>
2026-07-24 03:56:19 +0500

    Add Vercel Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { Analytics } from '@vercel/analytics/react'
 
 
 import '../styles.css'
@@ -46,6 +47,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Analytics />
         <Scripts />
       </body>
     </html>
